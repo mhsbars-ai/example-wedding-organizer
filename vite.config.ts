f86675f1a1d9991f7545def8a737a4cb0d785c95
mhsbars-ai <mahsabar99@gmail.com>
2026-07-23 15:40:57 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
-  return {
+  return {base: '/example-wedding-organizer/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
